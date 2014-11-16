5733d6b5df98006f64a0018551733534031745a0
eonarheim <erik.onarheim@gmail.com>
2014-11-16 17:52:12 -0600

    oops added game manager

---
+++ AntAICompetition/Server/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+
+namespace AntAICompetition.Server
+{
+    public class GameManager
+    {
+        private GameManager()
+        {
+        }
+
+        private static readonly GameManager _instance = new GameManager();
+        public Dictionary<int, Game> Games { get; set; }
+
+        public Game GetGame(int? id)
+        {
+            if (id.HasValue)
+            {
+                return Games[id.Value];
+            }
+            var game = new Game();
+            Games.Add(game.Id, game);
+            return game;
+        }
+
+        public static GameManager Instance
+        {
+            get { return _instance; }
+        }
+    }
+}
