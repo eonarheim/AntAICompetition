73d809be1abbc1a0ac04e52b886b4ccfe747c3d1
Erik Onarheim <erik.onarheim@gmail.com>
2015-01-07 01:31:35 -0600

    Close #5 implemented node demo agent

---
+++ NodeAgent/app.js
@@ -0,0 +1,57 @@
+﻿var AgentEngine = require('./agentengine.js');
+
+console.log('Starting Nodejs Demo agent');
+
+var directions = ["up", "down", "left", "right"];
+
+var getDirection = function(from, to) {
+    var dirX = to.X - from.X;
+    var dirY = to.Y - from.Y;
+    // greatest diff in x
+    var dir = "left";
+    if (Math.abs(dirX) > Math.abs(dirY)) {
+        dir = dirX > 0 ? "right" : "left";
+    }
+    else {
+        dir = dirY > 0 ? "down" : "up";
+    }
+    return dir;
+}
+
+var getRandomDirection = function() {
+    return directions[Math.floor(directions.length * Math.random())];
+}
+
+var getDistance = function(from, to) {
+    return Math.sqrt(Math.pow(from.X - to.X, 2) + Math.pow(from.Y - to.Y, 2));
+}
+
+var directionOfClosestFood = function (ant, foodList) {
+    var closest = null;
+    var minDistance = 9999.0;
+    for(var f in foodList){
+        if (getDistance(ant, foodList[f]) < minDistance) {
+            minDistance = getDistance(ant, foodList[f]);
+            closest = foodList[f];
+        }
+    }
+    if (closest != null) {
+        return getDirection(ant, closest);
+    }
+    else {
+        return getRandomDirection();
+    }
+    
+};
+
+var agentEngine = new AgentEngine(function (engine, gameState) {
+    // todo implement your awesome agent here :)
+    console.log("Current Turn", gameState.Turn, ": Time to next turn ", gameState.MillisecondsUntilNextTurn);
+    gameState.FriendlyAnts.forEach(function (a) {
+        engine.moveAnt(a, directionOfClosestFood(a, gameState.VisibleFood));
+    });
+    // Updates are sent to the server automagically at the end of mainloop!
+},'Node Agent', 'http://antsgame.azurewebsites.net');
+
+
+agentEngine.start();
---
+++ NodeAgent/promise.js
@@ -0,0 +1,175 @@
+﻿ /**
+    * Promises/A+ spec implementation of promises
+    * @class Promise
+    * @constructor
+    */
+var Promise = (function () {
+    function Promise() {
+        this._state = 2 /* Pending */;
+        this.successCallbacks = [];
+        this.rejectCallback = function () {
+        };
+    }
+    /**
+         * Wrap a value in a resolved promise
+         * @method wrap<T>
+         * @param [value=undefined] {T} An optional value to wrap in a resolved promise
+         * @returns Promise&lt;T&gt;
+         */
+        Promise.wrap = function (value) {
+        var promise = (new Promise()).resolve(value);
+        return promise;
+    };
+    /**
+         * Returns a new promise that resolves when all the promises passed to it resolve, or rejects
+         * when at least 1 promise rejects.
+         * @param promises {Promise[]}
+         * @returns Promise
+         */
+        Promise.join = function () {
+        var promises = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            promises[_i - 0] = arguments[_i];
+        }
+        var joinedPromise = new Promise();
+        if (!promises) {
+            return joinedPromise.resolve();
+        }
+        var total = promises.length;
+        var successes = 0;
+        var rejects = 0;
+        var errors = [];
+        promises.forEach(function (p) {
+            p.then(function () {
+                successes += 1;
+                if (successes === total) {
+                    joinedPromise.resolve();
+                }
+                else if (successes + rejects + errors.length === total) {
+                    joinedPromise.reject(errors);
+                }
+            }, function () {
+                rejects += 1;
+                if (successes + rejects + errors.length === total) {
+                    joinedPromise.reject(errors);
+                }
+            }).error(function (e) {
+                errors.push(e);
+                if ((errors.length + successes + rejects) === total) {
+                    joinedPromise.reject(errors);
+                }
+            });
+        });
+        return joinedPromise;
+    };
+    /**
+         * Chain success and reject callbacks after the promise is resovled
+         * @method then
+         * @param successCallback {T=>any} Call on resolution of promise
+         * @param rejectCallback {any=>any} Call on rejection of promise
+         * @returns Promise&lt;T&gt;
+         */
+        Promise.prototype.then = function (successCallback, rejectCallback) {
+        if (successCallback) {
+            this.successCallbacks.push(successCallback);
+            // If the promise is already resovled call immediately
+            if (this.state() === 0/* Resolved */) {
+                try {
+                    successCallback.call(this, this.value);
+                }
+                    catch (e) {
+                    this.handleError(e);
+                }
+            }
+        }
+        if (rejectCallback) {
+            this.rejectCallback = rejectCallback;
+            // If the promise is already rejected call immediately
+            if (this.state() === 1/* Rejected */) {
+                try {
+                    rejectCallback.call(this, this.value);
+                }
+                    catch (e) {
+                    this.handleError(e);
+                }
+            }
+        }
+        return this;
+    };
+    /**
+         * Add an error callback to the promise
+         * @method error
+         * @param errorCallback {any=>any} Call if there was an error in a callback
+         * @returns Promise&lt;T&gt;
+         */
+        Promise.prototype.error = function (errorCallback) {
+        if (errorCallback) {
+            this.errorCallback = errorCallback;
+        }
+        return this;
+    };
+    /**
+         * Resolve the promise and pass an option value to the success callbacks
+         * @method resolve
+         * @param [value=undefined] {T} Value to pass to the success callbacks
+         */
+        Promise.prototype.resolve = function (value) {
+        var _this = this;
+        if (this._state === 2/* Pending */) {
+            this.value = value;
+            try {
+                this._state = 0 /* Resolved */;
+                this.successCallbacks.forEach(function (cb) {
+                    cb.call(_this, _this.value);
+                });
+            }
+                catch (e) {
+                this.handleError(e);
+            }
+        }
+        else {
+            throw new Error('Cannot resolve a promise that is not in a pending state!');
+        }
+        return this;
+    };
+    /**
+         * Reject the promise and pass an option value to the reject callbacks
+         * @method reject
+         * @param [value=undefined] {T} Value to pass to the reject callbacks
+         */
+        Promise.prototype.reject = function (value) {
+        if (this._state === 2/* Pending */) {
+            this.value = value;
+            try {
+                this._state = 1 /* Rejected */;
+                this.rejectCallback.call(this, this.value);
+            }
+                catch (e) {
+                this.handleError(e);
+            }
+        }
+        else {
+            throw new Error('Cannot reject a promise that is not in a pending state!');
+        }
+        return this;
+    };
+    /**
+         * Inpect the current state of a promise
+         * @method state
+         * @returns PromiseState
+         */
+        Promise.prototype.state = function () {
+        return this._state;
+    };
+    Promise.prototype.handleError = function (e) {
+        if (this.errorCallback) {
+            this.errorCallback.call(this, e);
+        }
+        else {
+            throw e;
+        }
+    };
+    return Promise;
+})();
+
+module.exports = Promise;
