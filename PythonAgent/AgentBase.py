4e9beb938f184fff124961c62f7ee22f23b1ebf7
Andrew Nguyen <andrew.nguyen42@gmail.com>
2015-01-12 00:00:22 -0600

    THE PYTHON AGENT: IT LIVES

--- PythonAgent/AgentBase.py
+++ PythonAgent/AgentBase.py
@@ -1,48 +1,89 @@
 import requests
 import json
-from collections import namedtuple
+from time import sleep
+
 
 class AgentBase:
 
     def __init__(self, name, endpoint):
         self.__headers = {"Accept": "application/json", "Content-Type": "application/json"}
-        self.__name = name
         self.__endpoint = endpoint
-        self.__game_id = None
-        self.__auth_token = None
+        self.__pending_move_requests = []
+        self.game_id = None
+        self.time_to_next_turn = 0
+        self.auth_token = None
+        self.name = name
 
     def logon(self, game_id=None):
-        params = {'GameId': game_id, 'AgentName': self.__name}
+        params = {'GameId': game_id, 'AgentName': self.name}
         r = requests.post(self.__endpoint+'/api/game/logon', data=json.dumps(params), headers=self.__headers)
-        self.__game_id = r.json()['GameId']
-        self.__auth_token = r.json()['AuthToken']
+        self.game_id = r.json()['GameId']
+        self.auth_token = r.json()['AuthToken']
 
     def get_turn_info(self):
-        r = requests.get(self.__endpoint+'/api/game/{0}/turn'.format(self.__game_id), headers=self.__headers)
+        r = requests.get(self.__endpoint+'/api/game/{0}/turn'.format(self.game_id), headers=self.__headers)
         return self._json_object_hook(r.json())
 
     def get_game_info(self):
-        r = requests.post(self.__endpoint+'/api/game/{0}/status/{1}'.format(self.__game_id,self.__auth_token), headers=self.__headers)
+        r = requests.post(self.__endpoint+'/api/game/{0}/status/{1}'.format(self.game_id,self.auth_token), headers=self.__headers)
         data = r.json()
 
         friendly_ants = map(self.__parse_ant, data['FriendlyAnts'])
         enemy_ants = map(self.__parse_ant, data['EnemyAnts'])
         enemy_hills = map(self.__parse_hill, data['EnemyHills'])
         visible_food = map(self.__parse_food, data['VisibleFood'])
-
-
+        self.time_to_next_turn = data['MillisecondsUntilNextTurn']
 
         return GameStatus(data['IsGameOver'], data['Status'], data['GameId'], data['Turn'], data['TotalFood'],
                           self.__parse_hill(data['Hill']), data['FogOfWar'], data['MillisecondsUntilNextTurn'],
                           friendly_ants, enemy_ants, enemy_hills, visible_food, data['Walls'])
 
-    def update_game_state(self, move_ant_requests):
-        data = json.dumps({'AuthToken':self.__auth_token, 'GameId':self.__game_id, 'MoveAntRequests': move_ant_requests})
-        r = requests.post(self.__endpoint+'/api/game/{0}/status/{1}'.format(self.__game_id,self.__auth_token), headers=self.__headers)
+    def move_ant(self, ant, direction):
+        duplicate_request = reduce(lambda prev, nxt: prev and nxt.ant_id == ant.id, self.__pending_move_requests, False)
+        if duplicate_request:
+            print "WARNING! A move request has already been issued for ant {0}" .format(ant.id)
+            return False
 
+        self.__pending_move_requests.append(MoveAntRequest(ant, direction))
+        return True
 
-    def __json_object_hook(self, d):
-        return namedtuple('X', d.keys())(*d.values())
+    def update(self, game_state):
+        pass #UPDATE GAME LOGIC HERE
+
+    def start(self):
+        self.logon()
+        is_running = False
+
+        if not is_running:
+
+            is_running = True
+            while is_running:
+
+                gs = self.get_game_info()
+                if gs.is_game_over:
+                    is_running = False
+                    print("Game Over!")
+                    print(gs.status)
+                    break
+
+                self.update(gs)
+                self.send_update(self.__pending_move_requests)
+                self.__pending_move_requests = []
+
+                if self.time_to_next_turn > 0:
+                    sleep(self.time_to_next_turn/1000)
+
+    def send_update(self, move_ant_requests):
+        data = json.dumps({'AuthToken': self.auth_token, 'GameId': self.game_id,
+                           'MoveAntRequests': self.__serialize__ant_requests(move_ant_requests)})
+        r = requests.post(self.__endpoint+'/api/game/update'.format(self.game_id, self.auth_token), data=data, headers=self.__headers)
+
+    def __serialize__ant_requests(self, move_ant_requests):
+        ret_data = []
+        for move_ant_request in move_ant_requests:
+            ret_data.append({'AntId': move_ant_request.ant_id, 'Direction': move_ant_request.direction})
+        return ret_data
+
 
     def __parse_hill(self, hill_json):
         return Hill(hill_json['X'],hill_json['Y'],hill_json['Owner'])
@@ -79,6 +120,7 @@ class Ant:
         self.y = y
         self.owner = owner
 
+
 class Cell:
     def __init__(self, x, y, type, ant):
         self.x = x
@@ -110,9 +152,5 @@ class GameStatus:
         self.visible_food = visible_food
         self.walls = walls
 
-
-
-a = AgentBase('Python Test Agent', 'http://antsgame.azurewebsites.net')
-a.logon()
-print a.get_game_info()
-
+#agent = AgentBase("Python Agent", "http://antsgame.azurewebsites.net")
+#agent.start()
---
+++ PythonAgent/SampleAgent.py
@@ -0,0 +1,17 @@
+import AgentBase
+
+class SampleAgent(AgentBase.AgentBase):
+
+    def __init__(self, name, endpoint):
+        AgentBase.AgentBase.__init__(self, name, endpoint)
+
+    def update(self, game_status):
+
+        for ant in game_status.friendly_ants:
+            self.move_ant(ant.id, 'up') #this is a terrible strategy by the way
+
+agent = SampleAgent("Sample Python Agent", "http://antsgame.azurewebsites.net")
+agent.start()
+
+
+
